addfcbc87c2b96cb45f4355a4c411a7226827b0d
wenses penadillo lazares <wenses400@gmail.com>
2021-01-27 11:32:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,34 +3,35 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
-load_dotenv()
+def create_app():
 
-app = Flask(__name__)
-# Mongo Config
-client = MongoClient(os.environ.get("MONGODB_URI"))
-db = client.test
-users = db.users
-# User Config
-nombre = ""
-apellido = ""
+    load_dotenv()
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    global nombre
-    global apellido
-    if request.method == "POST":
-        user = request.form.get("usuario")
-        dni = request.form.get("dni_number")
-        if(users.find_one({"nombre": user, "DNI": dni})):
-            res = users.find_one({"nombre": user, "DNI": dni})
-            nombre = res["nombre"]
-            apellido = res["apellido"]
-            return redirect("home/")
-    return render_template("login.html")
+    app = Flask(__name__)
+    # Mongo Config
+    client = MongoClient(os.environ.get("MONGODB_URI"))
+    db = client.test
+    users = db.users
+    # User Config
+    nombre = ""
+    apellido = ""
 
-@app.route("/home/")
-def home():
-    return render_template("inicio.html", nombre=nombre, apellido=apellido)
+    @app.route("/", methods=["GET", "POST"])
+    def index():
+        global nombre
+        global apellido
+        if request.method == "POST":
+            user = request.form.get("usuario")
+            dni = request.form.get("dni_number")
+            if(users.find_one({"nombre": user, "DNI": dni})):
+                res = users.find_one({"nombre": user, "DNI": dni})
+                nombre = res["nombre"]
+                apellido = res["apellido"]
+                return redirect("home/")
+        return render_template("login.html")
 
-if __name__ == '__main__':
-    app.run()
+    @app.route("/home/")
+    def home():
+        return render_template("inicio.html", nombre=nombre, apellido=apellido)
+
+    return app
