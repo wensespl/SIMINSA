d9dd2f33243389bc496ed736388c57b4dbd6bd0a
wenses penadillo lazares <wenses400@gmail.com>
2021-02-13 19:54:33 -0500

    Añadiendo funcionalidad a Registrar
    Permite realizar la busqueda por DNI y mostrar datos y registrar paciente nuevo

--- DB.py
+++ DB.py
@@ -19,3 +19,52 @@ def buscarPaciente(dni):
     if(pacientes.find_one({"DNI": dni})):
         return True
     return False
+
+def guardarDatosPa(datos):
+    sintomas = {
+        "s1": "",
+        "s2": "",
+        "s3": "",
+        "s4": "",
+        "s5": "",
+        "s6": "",
+        "s7": ""
+    }
+    if datos.get("s1"):
+        sintomas['s1'] = "on"
+    if datos.get("s2"):
+        sintomas['s2'] = "on"
+    if datos.get("s3"):
+        sintomas['s3'] = "on"
+    if datos.get("s4"):
+        sintomas['s4'] = "on"
+    if datos.get("s5"):
+        sintomas['s5'] = "on"
+    if datos.get("s6"):
+        sintomas['s6'] = "on"
+    if datos.get("s7"):
+        sintomas['s7'] = "on"
+    pacientes.insert_one({
+        "DNI": datos['dni'],
+        "Nombre1": datos['nombre1'],
+        "Nombre2": datos['nombre2'],
+        "Apellido1": datos['apellido1'],
+        "Apellido2": datos['apellido2'],
+        "FN": datos['fecha_nacimiento'],
+        "Genero": datos['genero'],
+        "Telf": datos['telefono'],
+        "Dep": datos['departamento'],
+        "Dist": datos['distrito'],
+        "Calle": datos['calle'],
+        "Sintomas": sintomas,
+        "Otros": datos['otros'],
+        "EstPa": datos['estadop']
+    })
+    inmunizaciones.insert_one({
+        "DNI": datos['dni'],
+        "FV1": datos['fecha_vacuna1'],
+        "EV1": datos['estado_vacuna1'],
+        "Lab": datos['lab1'],
+        "FV2": datos['fecha_vacuna2'],
+        "EV2": datos['estado_vacuna2'],
+    })
--- app.py
+++ app.py
@@ -18,12 +18,6 @@ def load_user_data(usuario, contraseña):
     apellido = user["Apellidos"]
     tipe = user["Tipe"]
 
-def get_paciente_data(dni):
-    paciente = DB.pacientes.find_one({
-        "DNI": dni
-    })
-    return paciente
-
 def create_app():
     global nombre
     global apellido
@@ -62,18 +56,241 @@ def create_app():
 
     @app.route("/registrar/", methods=["GET", "POST"])
     def registrar():
-        kwargs = {
-            "nombre": nombre,
-            "apellido": apellido
-        }
+        dni_buscar = ""
+        dni_p = ""
+        nombre1_p = ""
+        nombre2_p = ""
+        apellido1_p = ""
+        apellido2_p = ""
+        est1_p = ""
+        est2_p = ""
+        M = ""
+        F = ""
+        telf_p = ""
+        fechana_p = ""
+        D1 = ""
+        D2 = ""
+        D3 = ""
+        D4 = ""
+        D5 = ""
+        D6 = ""
+        D7 = ""
+        D8 = ""
+        D9 = ""
+        D10 = ""
+        D11 = ""
+        D12 = ""
+        D13 = ""
+        D14 = ""
+        D15 = ""
+        D16 = ""
+        D17 = ""
+        D18 = ""
+        D19 = ""
+        D20 = ""
+        D21 = ""
+        D22 = ""
+        D23 = ""
+        D24 = ""
+        D25 = ""
+        dist_name_p = ""
+        calle_name_p = ""
+        s1_p = ""
+        s2_p = ""
+        s3_p = ""
+        s4_p = ""
+        s5_p = ""
+        s6_p = ""
+        s7_p = ""
+        otros_p = ""
+        estado1_v = ""
+        estado2_v = ""
+        estado3_v = ""
+        fv1 = ""
+        lab1 = ""
+        lab2 = ""
+        estado4_v = ""
+        estado5_v = ""
+        estado6_v = ""
+        fv2 = ""
 
         if request.method == "POST":
-            dnibuscar = request.form['dnibuscar']
-            if(dnibuscar):
+            """
+                if request.form.get("s1"):
+                    print("S1 checked")
+            """
+            if(request.form['boton'] == 'Buscar'):
+                dnibuscar = request.form['dnibuscar']
                 if(DB.buscarPaciente(dnibuscar)):
-                    res = get_paciente_data(dnibuscar)
-                    print(res["DNI"])
-            #obtener datos del formulario
+                    paciente = DB.pacientes.find_one({
+                        "DNI": dnibuscar
+                        })
+                    inmunizaciones = DB.inmunizaciones.find_one({
+                        "DNI": dnibuscar
+                    })
+                    #mostrar datos obtenidos
+                    dni_p = paciente['DNI']
+                    nombre1_p = paciente['Nombre1']
+                    nombre2_p = paciente['Nombre2']
+                    apellido1_p = paciente['Apellido1']
+                    apellido2_p = paciente['Apellido2']
+                    if(paciente['EstPa'] == "Estable"):
+                        est1_p = "selected"
+                    else:
+                        est2_p = "selected"
+                    if(paciente['Genero'] == "Masculino"):
+                        M = "selected"
+                    else:
+                        F = "selected"
+                    telf_p = paciente['Telf']
+                    fechana_p = paciente['FN']
+                    if(paciente['Dep'] == "Amazonas"):
+                        D1 = "selected"
+                    elif(paciente['Dep'] == "Ancash"):
+                        D2 = "selected"
+                    elif(paciente['Dep'] == "Apurímac"):
+                        D3 = "selected"
+                    elif(paciente['Dep'] == "Arequipa"):
+                        D4 = "selected"
+                    elif(paciente['Dep'] == "Ayacucho"):
+                        D5 = "selected"
+                    elif(paciente['Dep'] == "Cajamarca"):
+                        D6 = "selected"
+                    elif(paciente['Dep'] == "Callao"):
+                        D7 = "selected"
+                    elif(paciente['Dep'] == "Cusco"):
+                        D8 = "selected"
+                    elif(paciente['Dep'] == "Huancavelica"):
+                        D9 = "selected"
+                    elif(paciente['Dep'] == "Huánuco"):
+                        D10 = "selected"
+                    elif(paciente['Dep'] == "Ica"):
+                        D11 = "selected"
+                    elif(paciente['Dep'] == "Junín"):
+                        D12 = "selected"
+                    elif(paciente['Dep'] == "La Libertad"):
+                        D13 = "selected"
+                    elif(paciente['Dep'] == "Lambayeque"):
+                        D14 = "selected"
+                    elif(paciente['Dep'] == "Lima"):
+                        D15 = "selected"
+                    elif(paciente['Dep'] == "Loreto"):
+                        D16 = "selected"
+                    elif(paciente['Dep'] == "Madre de Dios"):
+                        D17 = "selected"
+                    elif(paciente['Dep'] == "Moquegua"):
+                        D18 = "selected"
+                    elif(paciente['Dep'] == "Pasco"):
+                        D19 = "selected"
+                    elif(paciente['Dep'] == "Piura"):
+                        D20 = "selected"
+                    elif(paciente['Dep'] == "Puno"):
+                        D21 = "selected"
+                    elif(paciente['Dep'] == "San martin"):
+                        D22 = "selected"
+                    elif(paciente['Dep'] == "Tacna"):
+                        D23 = "selected"
+                    elif(paciente['Dep'] == "Tumbes"):
+                        D24 = "selected"
+                    else:
+                        D25 = "selected"
+                    dist_name_p = paciente['Dist']
+                    calle_name_p = paciente['Calle']
+                    if(paciente['Sintomas']['s1'] == "on"):
+                        s1_p = "checked"
+                    if(paciente['Sintomas']['s2'] == "on"):
+                        s2_p = "checked"
+                    if(paciente['Sintomas']['s3'] == "on"):
+                        s3_p = "checked"
+                    if(paciente['Sintomas']['s4'] == "on"):
+                        s4_p = "checked"
+                    if(paciente['Sintomas']['s5'] == "on"):
+                        s5_p = "checked"
+                    if(paciente['Sintomas']['s6'] == "on"):
+                        s6_p = "checked"
+                    if(paciente['Sintomas']['s7'] == "on"):
+                        s7_p = "checked"
+                    otros_p = paciente['Otros']
+                    if(inmunizaciones['EV1'] == "Recibida"):
+                        estado1_v = "selected"
+                    elif(inmunizaciones['EV1'] == "No recibida"):
+                        estado2_v = "selected"
+                    else:
+                        estado3_v = "selected"
+                    fv1 = inmunizaciones['FV1']
+                    if(inmunizaciones['Lab'] == "Sinopharm"):
+                        lab1 = "selected"
+                    else:
+                        lab2 = "selected"
+                    if(inmunizaciones['EV2'] == "Recibida"):
+                        estado4_v = "selected"
+                    elif(inmunizaciones['EV2'] == "No recibida"):
+                        estado5_v = "selected"
+                    else:
+                        estado6_v = "selected"
+                    fv2 = inmunizaciones['FV2']
+            else:
+                DB.guardarDatosPa(request.form)
+                return redirect("registrar/")
+        kwargs = {
+            "dni_buscar": dni_buscar,
+            "dni_p": dni_p,
+            "nombre1_p": nombre1_p,
+            "nombre2_p": nombre2_p,
+            "apellido1_p": apellido1_p,
+            "apellido2_p": apellido2_p,
+            "est1_p": est1_p,
+            "est2_p": est2_p,
+            "M": M,
+            "F": F,
+            "telf_p": telf_p,
+            "fechana_p": fechana_p,
+            "D1": D1,
+            "D2": D2,
+            "D3": D3,
+            "D4": D4,
+            "D5": D5,
+            "D6": D6,
+            "D7": D7,
+            "D8": D8,
+            "D9": D9,
+            "D10": D10,
+            "D11": D11,
+            "D12": D12,
+            "D13": D13,
+            "D14": D14,
+            "D15": D15,
+            "D16": D16,
+            "D17": D17,
+            "D18": D18,
+            "D19": D19,
+            "D20": D20,
+            "D21": D21,
+            "D22": D22,
+            "D23": D23,
+            "D24": D24,
+            "D25": D25,
+            "dist_name_p": dist_name_p,
+            "calle_name_p": calle_name_p,
+            "s1_p": s1_p,
+            "s2_p": s2_p,
+            "s3_p": s3_p,
+            "s4_p": s4_p,
+            "s5_p": s5_p,
+            "s6_p": s6_p,
+            "s7_p": s7_p,
+            "otros_p": otros_p,
+            "estado1_v": estado1_v,
+            "estado2_v": estado2_v,
+            "estado3_v": estado3_v,
+            "fv1": fv1,
+            "lab1": lab1,
+            "lab2": lab2,
+            "estado4_v": estado4_v,
+            "estado5_v": estado5_v,
+            "estado6_v": estado6_v,
+            "fv2": fv2,
+        }
         return render_template("Registrar.html", **kwargs)
 
     return app
