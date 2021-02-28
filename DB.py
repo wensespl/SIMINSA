d216a9ecfabefbabac7bf197eeb1ae894872c939
wenses penadillo lazares <wenses400@gmail.com>
2021-02-27 20:26:48 -0500

    Update DB.py
    Añadiendo la funcionalidad de actualizar los datos desde monitoreo

--- DB.py
+++ DB.py
@@ -75,10 +75,62 @@ def guardarDatosPa(datos):
         "EV1": datos['estado_vacuna1'],
         "Lab": datos['lab1'],
         "FV2": datos['fecha_vacuna2'],
-        "EV2": datos['estado_vacuna2'],
+        "EV2": datos['estado_vacuna2']
     })
 
 def guardarDatosMo(datos):
-    reportes.insert_one({
-        "Rep": datos['rep_d']
+    if(not datos['rep_d'] == ""):
+        reportes.insert_one({
+            "Rep": datos['rep_d']
+        })
+    nombre = datos['nombresapellidos']
+    nombres = nombre.split(sep=' ')
+    sintomas = {
+        "s1": "",
+        "s2": "",
+        "s3": "",
+        "s4": "",
+        "s5": "",
+        "s6": "",
+        "s7": ""
+    }
+    if datos.get("s1"):
+        sintomas['s1'] = "on"
+    if datos.get("s2"):
+        sintomas['s2'] = "on"
+    if datos.get("s3"):
+        sintomas['s3'] = "on"
+    if datos.get("s4"):
+        sintomas['s4'] = "on"
+    if datos.get("s5"):
+        sintomas['s5'] = "on"
+    if datos.get("s6"):
+        sintomas['s6'] = "on"
+    if datos.get("s7"):
+        sintomas['s7'] = "on"
+    pacientes.find_one_and_update({
+        "Nombre1": nombres[0],
+        "Nombre2": nombres[1],
+        "Apellido1": nombres[2],
+        "Apellido2": nombres[3]
+        },
+        {'$set':{
+            "Sintomas": sintomas,
+            "Otros": datos['otros'],
+            "EstPa": datos['estado']
+        }})
+    paciente = pacientes.find_one({
+        "Nombre1": nombres[0],
+        "Nombre2": nombres[1],
+        "Apellido1": nombres[2],
+        "Apellido2": nombres[3]
     })
+    inmunizaciones.find_one_and_update({
+        "DNI": paciente['DNI']
+        },
+        {'$set':{
+            "FV1": datos['fecha_vacuna1'],
+            "EV1": datos['estado_vacuna1'],
+            "FV2": datos['fecha_vacuna2'],
+            "EV2": datos['estado_vacuna2']
+        }})
